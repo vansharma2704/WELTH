b3f69737416793b795adac73fb03d3187b48164b
vansharma2704 <vansh3649@gmail.com>
2025-11-12 03:06:56 +0530

    chore: Update npm dependencies

--- middleware.js
+++ middleware.js
@@ -32,6 +32,7 @@ const clerk=  clerkMiddleware(async (auth,req) => {
 export default createMiddleware(aj, clerk);
 
 export const config = {
+  runtime:'nodejs',
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
